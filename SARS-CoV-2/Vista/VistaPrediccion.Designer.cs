e73c759030ebdabe1564abfbf89fd2d450e40e2a
Fernando andres Lopez Carvajal <flopezc@alumnosuls.cl>
2021-12-08 19:53:09 -0300

    Escenarios Form, responsive

--- SARS-CoV-2/Vista/VistaPrediccion.Designer.cs
+++ SARS-CoV-2/Vista/VistaPrediccion.Designer.cs
@@ -35,17 +35,29 @@ namespace SARS_CoV_2.Vista
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.Grafico = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.cartesianChart1 = new LiveChartsCore.SkiaSharpView.WinForms.CartesianChart();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,7 +66,7 @@ namespace SARS_CoV_2.Vista
             this.Entrenar.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.Entrenar.Location = new System.Drawing.Point(0, 390);
             this.Entrenar.Name = "Entrenar";
-            this.Entrenar.Size = new System.Drawing.Size(119, 30);
+            this.Entrenar.Size = new System.Drawing.Size(128, 30);
             this.Entrenar.TabIndex = 1;
             this.Entrenar.Text = "Entrenar Red";
             this.Entrenar.UseVisualStyleBackColor = true;
@@ -64,7 +76,7 @@ namespace SARS_CoV_2.Vista
             this.Exportar.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.Exportar.Location = new System.Drawing.Point(0, 420);
             this.Exportar.Name = "Exportar";
-            this.Exportar.Size = new System.Drawing.Size(119, 30);
+            this.Exportar.Size = new System.Drawing.Size(128, 30);
             this.Exportar.TabIndex = 2;
             this.Exportar.Text = "Exportar Resultados";
             this.Exportar.UseVisualStyleBackColor = true;
@@ -73,85 +85,48 @@ namespace SARS_CoV_2.Vista
             // 
             this.label1.AutoSize = true;
             this.label1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.label1.Location = new System.Drawing.Point(19, 0);
+            this.label1.Location = new System.Drawing.Point(68, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 30);
+            this.label1.Size = new System.Drawing.Size(60, 30);
             this.label1.TabIndex = 3;
-            this.label1.Text = " Predicción en el  \r\n  Escenario Real";
+            this.label1.Text = "Escenario \r\n    Real\r\n";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(199, 360);
+            this.label2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label2.Location = new System.Drawing.Point(65, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(118, 30);
+            this.label2.Size = new System.Drawing.Size(63, 30);
             this.label2.TabIndex = 4;
-            this.label2.Text = "    Predicción en el \r\n  Escenario Optimista";
+            this.label2.Text = " Escenario \r\n Optimista";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(323, 360);
+            this.label3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label3.Location = new System.Drawing.Point(65, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(110, 30);
+            this.label3.Size = new System.Drawing.Size(63, 30);
             this.label3.TabIndex = 5;
-            this.label3.Text = "  Predicción en el \r\nEscenario Pesimista";
+            this.label3.Text = " Escenario \r\n Pesimista";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(439, 360);
+            this.label4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label4.Location = new System.Drawing.Point(57, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(121, 30);
+            this.label4.Size = new System.Drawing.Size(71, 30);
             this.label4.TabIndex = 6;
-            this.label4.Text = "   Predicción en el \r\nEscenario Modificado";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Red;
-            this.label5.Location = new System.Drawing.Point(416, 215);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(17, 15);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "--";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.label6.Location = new System.Drawing.Point(166, 360);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(17, 15);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "--";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.label7.Location = new System.Drawing.Point(300, 360);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(17, 15);
-            this.label7.TabIndex = 9;
-            this.label7.Text = "--";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.label8.Location = new System.Drawing.Point(416, 360);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(17, 15);
-            this.label8.TabIndex = 10;
-            this.label8.Text = "--";
+            this.label4.Text = "   Escenario \r\n Modificado";
             // 
             // Grafico
             // 
             this.Grafico.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.Grafico.Location = new System.Drawing.Point(0, 360);
             this.Grafico.Name = "Grafico";
-            this.Grafico.Size = new System.Drawing.Size(119, 30);
+            this.Grafico.Size = new System.Drawing.Size(128, 30);
             this.Grafico.TabIndex = 11;
             this.Grafico.Text = "Graficar Predicción";
             this.Grafico.UseVisualStyleBackColor = true;
@@ -164,26 +139,109 @@ namespace SARS_CoV_2.Vista
             this.panel1.Controls.Add(this.Entrenar);
             this.panel1.Controls.Add(this.Exportar);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(681, 0);
+            this.panel1.Location = new System.Drawing.Point(672, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(119, 450);
+            this.panel1.Size = new System.Drawing.Size(128, 450);
             this.panel1.TabIndex = 12;
             // 
             // panel3
             // 
-            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Controls.Add(this.panel6);
+            this.panel3.Controls.Add(this.panel7);
+            this.panel3.Controls.Add(this.panel4);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.Location = new System.Drawing.Point(0, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(119, 213);
+            this.panel3.Size = new System.Drawing.Size(128, 178);
             this.panel3.TabIndex = 12;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.pictureBox4);
+            this.panel5.Controls.Add(this.label4);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 132);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(128, 44);
+            this.panel5.TabIndex = 16;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox4.Image = global::SARS_CoV_2.Properties.Resources.green;
+            this.pictureBox4.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(59, 44);
+            this.pictureBox4.TabIndex = 17;
+            this.pictureBox4.TabStop = false;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.label3);
+            this.panel6.Controls.Add(this.pictureBox3);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel6.Location = new System.Drawing.Point(0, 88);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(128, 44);
+            this.panel6.TabIndex = 16;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox3.Image = global::SARS_CoV_2.Properties.Resources.yellow;
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(59, 44);
+            this.pictureBox3.TabIndex = 16;
+            this.pictureBox3.TabStop = false;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.label2);
+            this.panel7.Controls.Add(this.pictureBox2);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel7.Location = new System.Drawing.Point(0, 44);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(128, 44);
+            this.panel7.TabIndex = 15;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox2.Image = global::SARS_CoV_2.Properties.Resources.blue;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(59, 44);
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Controls.Add(this.pictureBox1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(128, 44);
+            this.panel4.TabIndex = 16;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Image = global::SARS_CoV_2.Properties.Resources.red;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(59, 44);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
             // 
             // cartesianChart1
             // 
             this.cartesianChart1.Dock = System.Windows.Forms.DockStyle.Top;
             this.cartesianChart1.Location = new System.Drawing.Point(0, 0);
             this.cartesianChart1.Name = "cartesianChart1";
-            this.cartesianChart1.Size = new System.Drawing.Size(681, 97);
+            this.cartesianChart1.Size = new System.Drawing.Size(672, 178);
             this.cartesianChart1.TabIndex = 0;
             // 
             // panel2
@@ -192,7 +250,7 @@ namespace SARS_CoV_2.Vista
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(681, 100);
+            this.panel2.Size = new System.Drawing.Size(672, 178);
             this.panel2.TabIndex = 13;
             // 
             // VistaPrediccion
@@ -202,22 +260,25 @@ namespace SARS_CoV_2.Vista
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Name = "VistaPrediccion";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Prediccion";
             this.panel1.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -228,14 +289,18 @@ namespace SARS_CoV_2.Vista
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Button Grafico;
         private System.Windows.Forms.Panel panel1;
         private LiveChartsCore.SkiaSharpView.WinForms.CartesianChart cartesianChart1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
