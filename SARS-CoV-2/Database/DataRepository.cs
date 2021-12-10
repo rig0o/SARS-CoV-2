a81d64c25eb0d5f1ed2b0aa0a30a8c1a88772329
rodrigo <rmamani@alumnosuls.cl>
2021-12-10 17:56:46 -0300

    Actualización de métodos

--- SARS-CoV-2/Database/DataRepository.cs
+++ SARS-CoV-2/Database/DataRepository.cs
@@ -51,42 +51,43 @@ namespace SARS_CoV_2.Database
                                               Delta = d.Delta
 
                                           };
-            lst = lst.TakeLast(20);
-
-            lst = lst.Take(10);
-
-
+            lst = lst.TakeLast(10);
+            //lst = lst.Take(10);
             return lst.ToList();
         }
         public List<DatasetDto> GetDataTrain()
         {
-            IEnumerable<DatasetDto> lst = from d in _dbcontext.Datasets
-                                          select new DatasetDto {
-                                           Fecha = d.Fecha,
-                                           CnuevoTotales = d.CnuevoTotales,
-                                           PcrDiarios = d.PcrDiarios,
-                                           MediaMovil = d.MediaMovil,
-                                           Refectivo = d.Refectivo,
-                                           PositividadPcr = d.PositividadPcr,
-                                           TasaTest = d.TasaTest,
-                                           PppConurbacioLaSerenaCoquimbo =d.PppConurbacioLaSerenaCoquimbo,
-                                           PppOvalle = d.PppOvalle,
-                                           PppIllapel =d.PppIllapel,
-                                           PppSalamanca = d.PppSalamanca,
-                                           PppMontePatria = d.PppMontePatria,
-                                           PrimeraDosis = d.PrimeraDosis,
-                                           SegundaDosis = d.SegundaDosis,
-                                           UnicaDosis = d.UnicaDosis,
-                                           RefuerzoDosis =d.RefuerzoDosis,
-                                           CconfirmadosAntigeno = d.CconfirmadosAntigeno,
-                                           PermisoVacaciones = d.PermisoVacaciones,
-                                           PaseMovilidad = d.PaseMovilidad,
-                                           EstadoExcepcion =d.EstadoExcepcion,
-                                           Alpha = d.Alpha,
-                                           Gamma = d.Gamma,
-                                           Delta = d.Delta
-
-                                         };
+            IEnumerable<DatasetDto> lst = from d in _dbcontext.Datasets.Take(462)
+                                          select new DatasetDto
+                                          {
+                                              Fecha = d.Fecha,
+                                              CnuevoTotales = d.CnuevoTotales,
+                                              CconfirmadosRecuperados = d.CconfirmadosRecuperados,
+                                              CactivosConfirmados = d.CactivosConfirmados,
+                                              CactivosProbables = d.CactivosProbables,
+                                              CsospechaReinfeccion = d.CsospechaReinfeccion,
+                                              PcrDiarios = d.PcrDiarios,
+                                              MediaMovil = d.MediaMovil,
+                                              Refectivo = d.Refectivo,
+                                              PositividadPcr = d.PositividadPcr,
+                                              TasaTest = d.TasaTest,
+                                              PppConurbacioLaSerenaCoquimbo = d.PppConurbacioLaSerenaCoquimbo,
+                                              PppOvalle = d.PppOvalle,
+                                              PppIllapel = d.PppIllapel,
+                                              PppSalamanca = d.PppSalamanca,
+                                              PppMontePatria = d.PppMontePatria,
+                                              PrimeraDosis = d.PrimeraDosis,
+                                              SegundaDosis = d.SegundaDosis,
+                                              UnicaDosis = d.UnicaDosis,
+                                              RefuerzoDosis = d.RefuerzoDosis,
+                                              CconfirmadosAntigeno = d.CconfirmadosAntigeno,
+                                              PermisoVacaciones = d.PermisoVacaciones,
+                                              PaseMovilidad = d.PaseMovilidad,
+                                              EstadoExcepcion = d.EstadoExcepcion,
+                                              Alpha = d.Alpha,
+                                              Gamma = d.Gamma,
+                                              Delta = d.Delta
+                                          };
             return lst.ToList();
         }
         public List<GraficoDto> GetDataTarget()
@@ -112,5 +113,25 @@ namespace SARS_CoV_2.Database
         //              };
         //    return lis;
         //}
+
+        public static double[,] DesNorm(double[,] x)
+        {
+            double[,] aux = new double[x.GetLength(0),x.GetLength(1)];
+
+            for (int i = 0; i < x.GetLength(0); i++)
+            {
+                for (int  j = 0;  j < x.GetLength(1);  j++)
+                {
+                    aux[i,j] = DesNorm(x[i,j]);  //return value * (max - min) + min;
+                }
+            }
+            return aux;
+        }
+        public static double DesNorm(double x)
+        {
+
+            return Math.Round(x * (385 - 4) + 4);  //return value * (max - min) + min;
+
+        }
     }
 }
